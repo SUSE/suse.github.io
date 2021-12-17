77d43ab1b0ebb502372f68a771a48fe6676acfc8
Dirk Müller <dirk@dmllr.de>
2021-12-17 17:13:27 +0100

    Deploying to gh-pages from @ SUSE/project-portal@1e5f82439bfdd61d14f268c5a4c90a9c683f7dd8 🚀

---
+++ _next/static/M1_lHr77ilKYcc7_NAQ_B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a4d5c4d24e93529f.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
