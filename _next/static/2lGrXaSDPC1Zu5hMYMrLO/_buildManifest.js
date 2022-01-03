7d4784129590315c5c6a7962c56e4c22687fa8ab
dirkmueller <dmueller@suse.com>
2022-01-03 08:37:47 +0000

    Deploying to main from @ SUSE/suse.github.io@0b2fc5a57f5d2e0fe473d0d5cdcea1fb4ce84574 🚀

---
+++ _next/static/2lGrXaSDPC1Zu5hMYMrLO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1079871f28c372ef.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
